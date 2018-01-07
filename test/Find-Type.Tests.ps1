091a85af5f2e74033845d61c8ea4110b2f7d026e
Patrick M. Meinecke <seeminglyscience@gmail.com>
2018-01-07 09:48:43 -0500

    Refactor tests
    - Replace custom assertion functions with custom assertion operators
    
    - Add tests for new argument completers, Find-Namespace, and new parameters

--- test/Find-Type.Tests.ps1
+++ test/Find-Type.Tests.ps1
@@ -12,7 +12,7 @@ Describe 'Find-Type tests' {
         It 'gets types from passed assemblies' {
             $results = Get-Assembly ClassExplorer | Find-Type
 
-            $results | ShouldAll { $_.Assembly.GetName().Name -eq 'ClassExplorer' }
+            $results | Should -All { $_.Assembly.GetName().Name -eq 'ClassExplorer' }
         }
         It 'gets the type of any other object passed' {
             Get-Item . | Find-Type | Should -Be ([System.IO.DirectoryInfo])
@@ -30,7 +30,7 @@ Describe 'Find-Type tests' {
 
         It 'accepts both a name and a script block as named parameters in the same command' {
             Find-Type -Name Runspace* -FilterScript { $_.IsAbstract } |
-                ShouldAny { $_ -eq [runspace] }
+                Should -Any { $_ -eq [runspace] }
         }
 
         It 'accepts namespace at position 1' {
@@ -50,45 +50,56 @@ Describe 'Find-Type tests' {
         $result.Count | Should -Be 1
         $result | Should -Be ([powershell])
     }
+
     It 'matches by name' {
         Find-Type RunspaceMode | Should -Be ([System.Management.Automation.RunspaceMode])
     }
+
     It 'matches by namespace' {
-        Find-Type -Namespace System.Timers | ShouldAll { $_.Namespace -eq 'System.Timers' }
+        Find-Type -Namespace System.Timers | Should -All { $_.Namespace -eq 'System.Timers' }
     }
+
     It 'matches by full name' {
         Find-Type -FullName System.Management.Automation.ProxyCommand |
             Should -Be ([System.Management.Automation.ProxyCommand])
     }
+
     It 'matches by name with regex' {
         Find-Type "Runspace(Factory|ConnectionInfo)" -RegularExpression |
             Should -Be ([runspacefactory], [System.Management.Automation.Runspaces.RunspaceConnectionInfo])
     }
+
     It 'matches by namespace with regex' {
         Find-Type MethodAttributes -Namespace 'System\.(?!Reflection).+' -RegularExpression |
             Should -Be ([System.Management.Automation.Language.MethodAttributes])
     }
+
     It 'matches by full name with regex' {
         Find-Type -FullName 'System\.(?!Threading)\w+\.Timer$' -RegularExpression |
             Should -Be ([System.Timers.Timer])
     }
+
     It 'matches by base class' {
         $results = Find-Type -InheritsType System.Management.Automation.Language.Ast
 
-        $results | ShouldAll { $_.IsSubclassOf([System.Management.Automation.Language.Ast]) }
+        $results | Should -All { $_.IsSubclassOf([System.Management.Automation.Language.Ast]) }
         $results | Should -Not -BeNullOrEmpty
     }
+
     It 'matches by interface' {
         Find-Type -ImplementsInterface System.Collections.IList |
-            ShouldAll { $_.ImplementedInterfaces -contains [System.Collections.IList] }
+            Should -All { $_.ImplementedInterfaces -contains [System.Collections.IList] }
     }
+
     It 'filters to only interfaces' {
-        Find-Type -Interface | ShouldAll { $_.IsInterface }
+        Find-Type -Interface | Should -All { $_.IsInterface }
     }
+
     It 'filters to only abstract' {
-        Find-Type -Abstract | ShouldAll { $_.IsAbstract }
+        Find-Type -Abstract | Should -All { $_.IsAbstract }
     }
+
     It 'filters to only value types' {
-        Find-Type -ValueType | ShouldAll { $_.IsValueType }
+        Find-Type -ValueType | Should -All { $_.IsValueType }
     }
 }
